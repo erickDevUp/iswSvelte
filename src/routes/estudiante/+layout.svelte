cbd370cd635c00a7331e832088f9a9a6efa3dfb2
erickdevup <erickdevup@gmail.com>
2024-06-22 21:15:36 +0200

    fix router and layout

---
+++ src/routes/estudiante/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../../styles/in.css';
+	import '../../styles/inE.css';
+
+	import '../../styles/themify-icons.css';
+	import '../../styles/fonts.css';
+	import LinkMenu from '../../components/LinkMenu.svelte';
+	import Nav from '../../components/Nav.svelte';
+</script>
+
+<body>
+	<Nav></Nav>
+	<main>
+		<LinkMenu />
+		<slot />
+	</main>
+</body>
---
+++ src/routes/estudiante/recurso-educativo/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import RecursoEducativo from "../../../components/RecursoEducativo.svelte"
+
+</script>
+
+    <section>
+      <div>
+        <div class="section__header">
+          <h1>Recursos Educativos</h1>
+        </div>
+      </div>
+      <div class="cont">
+        <RecursoEducativo/>
+        <RecursoEducativo/>
+        <RecursoEducativo/>
+      </div>
+       
+    </section>
