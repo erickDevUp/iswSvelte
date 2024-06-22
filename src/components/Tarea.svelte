ebe31db1ff8c8ada7f6987982b5b61c181d5c1fc
erickdevup <erickdevup@gmail.com>
2024-06-22 22:53:35 +0200

    add modal to upload task

---
+++ src/components/Tarea.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Alert from './modals/Alert.svelte';
+    let isShowDelete = false;
+</script>
+
+<div class="cont__nt">
+	<div class="nt__t">
+		<div class="t__d">
+			<img src="/img/illustration-checklist.jpg" alt="checklist" />
+			<p>Nombre Tarea</p>
+		</div>
+	</div>
+	<div class="nt__d">
+		<p>Asunto Lorem ipsum dolor sit amet.</p>
+		<p>nombre orientacion</p>
+	</div>
+	<div class="header__buttom card__button">
+		<button><span class="ti-pencil-alt"></span></button>
+		<button on:click={() => isShowDelete = true}><span class="ti-trash"></span></button>
+		<button><span class="ti-import"></span></button>
+	</div>
+</div>
+
+<Alert on:click={() => isShowDelete = false} mensaje="Esto es un mensaje" isShow={isShowDelete}/>
---
+++ src/components/modals/AddTask.svelte
@@ -0,0 +1,42 @@
+<script>
+	import '../../styles/vm.css';
+	import Layout from './layout.svelte';
+	export let isShow;
+	let hasFiles = false;
+
+	function handleFileInput(event) {
+		const files = event.target.files;
+		if (files.length > 0) {
+			hasFiles = true;
+		} else {
+			hasFiles = false;
+		}
+	}
+	//en img necesito un check img
+</script>
+
+<Layout {isShow}>
+	<div class="modal">
+		<div class="modal__t">
+			<h1>Nueva Tarea</h1>
+			<button on:click><span class="ti-close"></span></button>
+		</div>
+		<hr />
+		<div class="content">
+			<input type="text" placeholder="Asunto" />
+			<div class="drag__area">
+				{#if hasFiles}
+					<h2>Archivo seleccionado exitosamente</h2>
+					<button>Cambiar archivo</button>
+				{:else}
+					<h2>Arrastra y suelta aquí</h2>
+					<span>o</span>
+					<button>Seleciona un archivo</button>
+				{/if}
+				<input type="file" id="input-file" class="addFile" multiple on:change={handleFileInput} />
+			</div>
+			<div id="preview"></div>
+			<button>Enviar</button>
+		</div>
+	</div>
+</Layout>
