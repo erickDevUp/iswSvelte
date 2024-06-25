fb85532209d12cb8218448142a27ba556aaf645e
erickdevup <erickdevup@gmail.com>
2024-06-25 21:56:33 +0200

    gandle globar orientation data

---
+++ src/store/orientacionesStore.js
@@ -0,0 +1,23 @@
+import { writable } from 'svelte/store';
+
+// Crear un store con un valor inicial vacío
+export const orientacionesStore = writable([]);
+
+export function editarOrientacion(id, nuevosDatos) {
+    orientacionesStore.update(orientaciones => {
+        return orientaciones.map(orientacion => {
+            if (orientacion.id === id) {
+                // Retorna la nueva versión de la orientación con los datos actualizados
+                return {...orientacion,...nuevosDatos };
+            } else {
+                // Retorna la orientación sin cambios si el id no coincide
+                return orientacion;
+            }
+        });
+    });
+}
+export function eliminarOrientacion(id) {
+    orientacionesStore.update(orientaciones => {
+        return orientaciones.filter(orientacion => orientacion.id!== id);
+    });
+}
