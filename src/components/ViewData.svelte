038b68a14c3ffda2448d032739db09136bca10bb
erickdevup <erickdevup@gmail.com>
2024-06-25 21:52:38 +0200

    custom view data

--- src/components/ViewData.svelte
+++ src/components/ViewData.svelte
@@ -1,13 +1,21 @@
+<script>
+	export let nombre;
+	export let tipo = '';
+	export let fileUrl;
+</script>
+
 <div class="cont__nt">
-    <div class="nt__t">
-      <div class="t__d">
-        <p>Nombre del Recurso Educativo</p>
-      </div>
-    </div>
-    <div class="nt__d">
-      <p>
-        tipo
-      </p>
-    </div>
-    <button><span class="ti-import"></span></button>
-  </div>
+	<div class="nt__t">
+		<div class="t__d">
+			<p>{nombre}</p>
+		</div>
+	</div>
+	<div class="nt__d">
+		<p>
+			{tipo}
+		</p>
+	</div>
+	<a href={fileUrl} target="_blank" download>
+		<button><span class="ti ti-download"></span></button>
+	</a>
+</div>
