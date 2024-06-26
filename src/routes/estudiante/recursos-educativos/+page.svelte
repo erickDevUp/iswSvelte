73b11867a1a0b11bb2a23e1564dfdfd484673ba6
erickdevup <erickdevup@gmail.com>
2024-06-26 13:59:41 +0200

    connect estudiante RED

--- src/routes/estudiante/recursos-educativos/+page.svelte
+++ src/routes/estudiante/recursos-educativos/+page.svelte
@@ -1,18 +1,31 @@
 <script>
-  import RecursoEducativo from "../../../components/ViewData.svelte"
+	import { onMount } from 'svelte';
+	import RecursoEducativo from '../../../components/ViewData.svelte';
+	import { cargarRecursoED } from '../../../helpers/cargarRecursoED';
+	import { recursoEDStore } from '../../../store/recursoEDStore';
+	import Search from '../../../components/Search.svelte';
 
+	onMount(async () => {
+		await cargarRecursoED();
+	});
+
+	const fetchUrl = 'recursoEducativo/';
 </script>
 
-    <section>
-      <div>
-        <div class="section__header">
-          <h1>Recursos Educativos</h1>
-        </div>
-      </div>
-      <div class="cont">
-        <RecursoEducativo/>
-        <RecursoEducativo/>
-        <RecursoEducativo/>
-      </div>
-       
-    </section>
+<section>
+  
+<Search url={fetchUrl} getObject={cargarRecursoED} store={recursoEDStore} />
+	<div>
+		<div class="section__header">
+			<h1>Recursos Educativos</h1>
+		</div>
+	</div>
+  <div class="cont">
+    {#each $recursoEDStore as recursoED (recursoED.id)}
+			<RecursoEducativo
+				nombre={recursoED.nombre}
+				id={recursoED.id}
+				fileUrl={recursoED.archivo}
+			/>
+		{/each}
+</section>
