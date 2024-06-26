095fa9f55223dce71a091250759aad70a61cec85
erickdevup <erickdevup@gmail.com>
2024-06-26 13:58:04 +0200

    customized Viewdata

--- src/components/ViewData.svelte
+++ src/components/ViewData.svelte
@@ -1,19 +1,30 @@
 <script>
+	import { parseFecha } from '../helpers/parseFecha';
 	export let nombre;
 	export let tipo = '';
+	export let id = '';
 	export let fileUrl;
+	export let fechaEntrega = null;
 </script>
 
 <div class="cont__nt">
 	<div class="nt__t">
 		<div class="t__d">
+			<img src="/img/illustration-checklist.jpg" alt="checklist" />
 			<p>{nombre}</p>
 		</div>
 	</div>
 	<div class="nt__d">
-		<p>
-			{tipo}
-		</p>
+		{#if tipo}
+			<p>Tipo: <span style="color: #007bff;">{tipo}</span></p>
+		{/if}
+		{#if id}
+			<p>Recurso #: <span style="color: #007bff;">{id}</span></p>
+		{/if}
+		{#if fechaEntrega}
+			<p>Fecha de Entrega: <span style="color: #007bff;">{parseFecha(fechaEntrega)}</span></p>
+		{/if}
+		
 	</div>
 	<a href={fileUrl} target="_blank" download>
 		<button><span class="ti ti-download"></span></button>
