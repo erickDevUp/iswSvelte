4db74b6b84795b79a4419c139246b5d6d143b44a
erickdevup <erickdevup@gmail.com>
2024-06-26 13:56:07 +0200

    custom link router

--- src/components/LinkMenu.svelte
+++ src/components/LinkMenu.svelte
@@ -1,15 +1,28 @@
 <script>
-     import {showMenu} from '../store/showMenu'
-  let isShow;
-  
-  showMenu.subscribe(value => {
-    isShow = value;
-  });
+	import { showMenu } from '../store/showMenu';
+	import { routes } from '../consts';
+  import { onMount } from 'svelte';
+  import { roles } from '../consts';
+
+  let selectedRoute = routes.profesor;
+	onMount(async () => {
+		const rol = localStorage.getItem('rol');
+		selectedRoute = roles.profesor == rol ? routes.profesor : routes.estudiante;
+	})
+
+	let isShow;
+
+	showMenu.subscribe((value) => {
+		isShow = value;
+	});
 </script>
-<nav class={` link-menu link-menu${isShow?"":"-hidden"}`}>
-    <div>
-      <a href="/estudiante/orientaciones"><span class="ti-agenda"></span>Orientaciones</a>
-      <a href="/estudiante/tareas"><span class="ti-book"></span>Tareas</a>
-      <a href="/estudiante/recursos-educativos"><span class="ti-desktop"></span>Recursos Educativos</a>
-    </div>
-  </nav>
+
+<nav class={` link-menu link-menu${isShow ? '' : '-hidden'}`}>
+	<div>
+		<a href={selectedRoute.orientaciones}><samp class="ti-agenda"></samp>Orientaciones</a>
+		<a href={selectedRoute.tareas}><span class="ti-book"></span>Tareas</a>
+		<a href={selectedRoute.recursosEducativos}
+			><span class="ti-desktop"></span>Recursos Educativos</a
+		>
+	</div>
+</nav>
--- src/consts.js
+++ src/consts.js
@@ -1,3 +1,18 @@
 
 export const roles = { "admin": 'admin', "alumno": 'alumno', "profesor": 'profesor' };
 export const apiUrl = 'http://127.0.0.1:8000/';
+
+export const routes = {
+    estudiante: {
+        orientaciones: '/estudiante/orientaciones',
+        tareas: '/estudiante/tareas',
+        recursosEducativos: '/estudiante/recursos-educativos',
+    },
+    profesor: {
+        orientaciones: '/profesor/orientaciones',
+        tareas: '/profesor/tareas',
+        recursosEducativos: '/profesor/recursos-educativos',
+    },
+}
+
+
