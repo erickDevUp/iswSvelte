04e4d81ea89a5cd73ccf2c4f8d1905124e112a4f
erickdevup <erickdevup@gmail.com>
2024-06-30 15:54:00 +0200

    customized delete

---
+++ src/components/modals/Delete.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Layout from './layout.svelte';
+
+	import { createEventDispatcher } from 'svelte';
+	export let mensaje;
+	export let isShow;
+
+	const dispatch = createEventDispatcher();
+
+	function handleClickCheck() {
+		dispatch('check', { mensaje, isShow });
+	}
+
+	function handleClickClose() {
+		dispatch('close', { mensaje, isShow });
+	}
+</script>
+
+<Layout {isShow}>
+	<div class={'alert'}>
+		<div class="alert__e">
+			<span class="ti-alert"></span>
+			<p>{mensaje || "¿Está seguro que desea eliminar?"}</p>
+		</div>
+		<div class="alert__b">
+			<button class="ti-check" on:click|preventDefault={handleClickCheck}></button>
+			<button class="ti-close" on:click={handleClickClose}></button>
+		</div>
+	</div>
+</Layout>
