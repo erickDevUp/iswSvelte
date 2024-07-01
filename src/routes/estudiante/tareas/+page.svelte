a7d8eae5014d6e1aea4e469b751ec45cb9154953
erickdevup <erickdevup@gmail.com>
2024-07-01 05:46:17 +0200

    change add task

--- src/routes/estudiante/tareas/+page.svelte
+++ src/routes/estudiante/tareas/+page.svelte
@@ -1,25 +1,39 @@
 <script>
+	import Tarea from '../../../components/WriteTarea.svelte';
+	import AddTask from '../../../components/modals/AddTask.svelte';
+	import { tareaStore } from '../../../store/tareaStore';
+	import { cargarTarea } from '../../../helpers/cargarTarea';
 
-	import Tarea from "../../../components/Tarea.svelte";
-	import AddTask from "../../../components/modals/AddTask.svelte";
+	import { onMount } from 'svelte';
 
-    let isShowAddTask = false;
+	onMount(async () => {
+		await cargarTarea();
+	});
+	let fetchUrl = "gestionarTarea/"
+	let isShowAddTask = false;
 </script>
+
 <section>
 	<div>
 		<div class="section__header">
 			<h1>Tareas</h1>
 			<div class="header__buttom">
-				<button on:click={() => isShowAddTask = true}><span class="ti-plus"></span></button>
-				
+				<button on:click={() => (isShowAddTask = true)}><span class="ti-plus"></span></button>
 			</div>
 		</div>
 	</div>
 	<div class="cont">
-        <Tarea />
-        <Tarea />
-        <Tarea />
-        <Tarea />
+		{#each $tareaStore as tarea (tarea.id)}
+			<Tarea
+				nota={tarea.nota}
+				nombreTarea={tarea.nombre}
+				nombreOrientacion={tarea.Orientaciones.nombre}
+				tipo={tarea.tipo}
+				fileUrl={tarea.tareaArchivo}
+				id={tarea.id}
+				fechaEntrega={tarea.Orientaciones.fechaEntrega}
+			/>
+		{/each}
 	</div>
-    <AddTask on:click={() => isShowAddTask = false} isShow={isShowAddTask}/>
+	<AddTask pageName={"Tarea"} method="POST" on:click={() => (isShowAddTask = false)} isShow={isShowAddTask} url={fetchUrl} />
 </section>
